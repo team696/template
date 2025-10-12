9b124cb45d5831fd420792693ca2ee38f30568f0
MelonzBigBoss <umadorwut913@gmail.com>
2025-10-12 09:59:05 -0700

    first upload

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "commandbasedskeleton";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = -1;
+  public static final String GIT_SHA = "UNKNOWN";
+  public static final String GIT_DATE = "UNKNOWN";
+  public static final String GIT_BRANCH = "UNKNOWN";
+  public static final String BUILD_DATE = "2025-10-12 09:51:21 PDT";
+  public static final long BUILD_UNIX_TIME = 1760287881745L;
+  public static final int DIRTY = 129;
+
+  private BuildConstants(){}
+}
