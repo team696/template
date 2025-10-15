c5f5bb8d341a0693675a9f59fc83882b79106a30
MelonzBigBoss <umadorwut913@gmail.com>
2025-10-15 15:02:16 -0700

    Added Debug jvmArgs

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "template";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 1;
-  public static final String GIT_SHA = "9b124cb45d5831fd420792693ca2ee38f30568f0";
-  public static final String GIT_DATE = "2025-10-12 09:59:05 PDT";
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "9ade87ee078582fa8e2e10a70a24948470639256";
+  public static final String GIT_DATE = "2025-10-12 23:28:09 PDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-10-12 23:26:11 PDT";
-  public static final long BUILD_UNIX_TIME = 1760336771416L;
+  public static final String BUILD_DATE = "2025-10-13 14:30:47 PDT";
+  public static final long BUILD_UNIX_TIME = 1760391047109L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
