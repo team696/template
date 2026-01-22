278ac6a7ac5e0f14d0bb3d83cdbf443e717fb1f6
MelonzBigBoss <umadorwut913@gmail.com>
2026-01-21 16:54:32 -0800

    fix actions

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "template";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 7;
-  public static final String GIT_SHA = "508e830e843fa3645e65bb0452051463aa80d655";
-  public static final String GIT_DATE = "2026-01-21 16:01:12 PST";
+  public static final int GIT_REVISION = 8;
+  public static final String GIT_SHA = "697d8254c8de031967616318194b71a79adcb0ab";
+  public static final String GIT_DATE = "2026-01-21 16:32:49 PST";
   public static final String GIT_BRANCH = "2026";
-  public static final String BUILD_DATE = "2026-01-21 16:09:23 PST";
-  public static final long BUILD_UNIX_TIME = 1769040563221L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-01-21 16:33:03 PST";
+  public static final long BUILD_UNIX_TIME = 1769041983378L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
